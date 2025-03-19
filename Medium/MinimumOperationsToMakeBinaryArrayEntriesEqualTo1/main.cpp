804dfebf42bc01153a606df86c318569587ffd7c
Daniel Liu <daniel17liu@gmail.com>
2025-03-19 08:21:05 -0700

    MinimumOperationsToMakeBinaryArrayEntriesEqualTo1

---
+++ Medium/MinimumOperationsToMakeBinaryArrayEntriesEqualTo1/main.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --------------------Leetcode-Data-Structures--------------------
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(vector<int> nums)
+{
+    ListNode *head = new ListNode();
+    ListNode *temp = head;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        head->val = nums[i];
+        if (i != nums.size() - 1)
+        {
+            head->next = new ListNode();
+            head = head->next;
+        }
+    }
+    return temp;
+}
+
+void printListNode(ListNode *head)
+{
+    ListNode *cur = head;
+    while (cur)
+    {
+        cout << cur->val << ' ';
+        cur = cur->next;
+    }
+    cout << endl;
+    return;
+}
+
+int sizeListNode(ListNode *head)
+{
+    int ans = 0;
+    ListNode *cur = head;
+    while (cur)
+    {
+        cur = cur->next;
+        ans++;
+    }
+    return ans;
+}
+
+// --------------------Snippet-Ends--------------------------------
+
+int minOperations(vector<int>& nums) {
+    deque<int> dq;
+    for (int &i : nums) {
+        dq.push_back(i);
+    }
+
+    int ans = 0;
+
+    while (!dq.empty()) {
+        int cur = dq.front();
+        dq.pop_front();
+        if (cur == 0 && dq.size() < 2) {
+            return -1;
+        }
+
+        if (cur == 0) {
+            ans++;
+            int n1 = dq.front();
+            dq.pop_front();
+            int n2 = dq.front();
+            dq.pop_front();
+
+            dq.push_front(!n2);
+            dq.push_front(!n1);
+        }
+    }
+    return ans;
+}
+
+signed main() {
+
+}
