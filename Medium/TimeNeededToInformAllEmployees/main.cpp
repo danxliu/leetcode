4c1926eebdb6550368947f58efd5693618713937
Daniel Liu <daniel17liu@gmail.com>
2025-03-19 09:16:35 -0700

    TimeNeededToInformAllEmployees

---
+++ Medium/TimeNeededToInformAllEmployees/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --------------------Leetcode-Data-Structures--------------------
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(vector<int> nums)
+{
+    ListNode *head = new ListNode();
+    ListNode *temp = head;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        head->val = nums[i];
+        if (i != nums.size() - 1)
+        {
+            head->next = new ListNode();
+            head = head->next;
+        }
+    }
+    return temp;
+}
+
+void printListNode(ListNode *head)
+{
+    ListNode *cur = head;
+    while (cur)
+    {
+        cout << cur->val << ' ';
+        cur = cur->next;
+    }
+    cout << endl;
+    return;
+}
+
+int sizeListNode(ListNode *head)
+{
+    int ans = 0;
+    ListNode *cur = head;
+    while (cur)
+    {
+        cur = cur->next;
+        ans++;
+    }
+    return ans;
+}
+
+// --------------------Snippet-Ends--------------------------------
+
+int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+    int ans = 0;
+    vector<vector<int>> adjmat(n, vector<int>());
+
+    for (int i=0; i<manager.size(); i++) {
+        if (manager[i] == -1) continue;
+        adjmat[manager[i]].push_back(i);
+    }
+
+    queue<pair<int, int>> q;
+    q.push({headID, 0});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int time = q.front().second;
+        q.pop();
+        ans = max(time, ans);
+
+        for (int i=0; i<adjmat[cur].size(); i++) {
+            q.push({adjmat[cur][i], time + informTime[cur]});
+        }
+    }
+    return ans; 
+}
+
+signed main() {
+
+}
