e82d88af2d58c3e2047fb0e21c7417aa200c181b
Daniel Liu <daniel17liu@gmail.com>
2025-03-20 16:09:03 -0700

    MaxConsecutiveOnesIII

---
+++ Medium/MaxConsecutiveOnesIII/main.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --------------------Leetcode-Data-Structures--------------------
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(vector<int> nums)
+{
+    ListNode *head = new ListNode();
+    ListNode *temp = head;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        head->val = nums[i];
+        if (i != nums.size() - 1)
+        {
+            head->next = new ListNode();
+            head = head->next;
+        }
+    }
+    return temp;
+}
+
+void printListNode(ListNode *head)
+{
+    ListNode *cur = head;
+    while (cur)
+    {
+        cout << cur->val << " ";
+        cur = cur->next;
+    }
+    cout << "\n";
+    return;
+}
+
+// --------------------Snippet-Ends--------------------------------
+
+int longestOnes(vector<int>& nums, int k) {
+    int cur = 0;
+    int j = 0;
+    int ans = 0;
+    for (int i=0; i<nums.size(); i++) {
+        if (nums[i] == 0) {
+            cur++;
+        }
+        while (cur > k) {
+            if (nums[j] == 0) {
+                cur--;
+            }
+            j++;
+        }
+        ans = max(ans, i - j + 1);
+    }
+    return ans;
+}
