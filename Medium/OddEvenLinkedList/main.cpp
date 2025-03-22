630f5ed88f5b3dd62ee231bfa44b0a51b7e4736c
Daniel Liu <daniel17liu@gmail.com>
2025-03-22 12:58:20 -0700

    OddEvenLinkedList

---
+++ Medium/OddEvenLinkedList/main.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --------------------Leetcode-Data-Structures--------------------
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(vector<int> nums)
+{
+    ListNode *head = new ListNode();
+    ListNode *temp = head;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        head->val = nums[i];
+        if (i != nums.size() - 1)
+        {
+            head->next = new ListNode();
+            head = head->next;
+        }
+    }
+    return temp;
+}
+
+void printListNode(ListNode *head)
+{
+    ListNode *cur = head;
+    while (cur)
+    {
+        cout << cur->val << ' ';
+        cur = cur->next;
+    }
+    cout << endl;
+    return;
+}
+
+int sizeListNode(ListNode *head)
+{
+    int ans = 0;
+    ListNode *cur = head;
+    while (cur)
+    {
+        cur = cur->next;
+        ans++;
+    }
+    return ans;
+}
+
+// --------------------Snippet-Ends--------------------------------
+
+ListNode *oddEvenList(ListNode *head)
+{
+    if (!head || !head->next)
+        return head;
+
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+
+    while (even && even->next) {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenHead;
+
+    return head;
+}
+
+signed main()
+{
+    vector<int> list = {1, 2, 3, 4, 5};
+    ListNode *head = createListNode(list);
+    ListNode *res = oddEvenList(head);
+    printListNode(res);
+}
