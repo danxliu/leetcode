f06b0a2cec5a0f5ff45c5b9aeb995932341ae84e
Daniel Liu <daniel17liu@gmail.com>
2025-03-23 09:14:19 -0700

    LongestZigZagPathInBinaryTree

---
+++ Medium/LongestZigZagPathInBinaryTree/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --------------------Leetcode-Data-Structures--------------------
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(vector<int> nums)
+{
+    ListNode *head = new ListNode();
+    ListNode *temp = head;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        head->val = nums[i];
+        if (i != nums.size() - 1)
+        {
+            head->next = new ListNode();
+            head = head->next;
+        }
+    }
+    return temp;
+}
+
+void printListNode(ListNode *head)
+{
+    ListNode *cur = head;
+    while (cur)
+    {
+        cout << cur->val << ' ';
+        cur = cur->next;
+    }
+    cout << endl;
+    return;
+}
+
+int sizeListNode(ListNode *head)
+{
+    int ans = 0;
+    ListNode *cur = head;
+    while (cur)
+    {
+        cur = cur->next;
+        ans++;
+    }
+    return ans;
+}
+
+// --------------------Snippet-Ends--------------------------------
+
+int len = 0;
+void helper(TreeNode *root, bool left, int cur)
+{
+    if (!root)
+        return;
+
+    len = max(len, cur);
+    if (left) {
+        helper(root->left, false, cur+1);
+        helper(root->right, true, 1);
+    }
+    else {
+        helper(root->left, false, 1);
+        helper(root->right, true, cur+1);
+    }
+        
+}
+
+int longestZigZag(TreeNode *root)
+{
+    if (!root) return 0;
+    helper(root, true, 0);
+    return len;
+}
+
+signed main()
+{
+}
